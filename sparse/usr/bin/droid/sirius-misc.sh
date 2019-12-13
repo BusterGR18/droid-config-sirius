a16a2c5f3ea501df20ebb5d871cd548f94c4c407
BusterGR18 <bustergb18@gmail.com>
2019-12-13 01:50:51 +0000

    merge changes
    Change-Id: Ice59f73a595dc3743762c6ee268f6a1839642f77

--- sparse/usr/bin/droid/sirius-misc.sh
+++ sparse/usr/bin/droid/sirius-misc.sh
@@ -4,6 +4,14 @@
 # We're done here if this isn't the first boot
 [ ! -f /var/tmp/make-droid-links ] && exit 0
 
+# Android Storage linking
+m_path="/data/media"
+[ -d "$m_path/0" ] && m_path+="/0"
+
+rm -f /home/nemo/android_storage /sdcard
+ln -s $m_path /home/nemo/android_storage
+ln -s $m_path /sdcard
+
 # Misc first boot stuff
 chown -R nemo: /home/nemo/
 gpasswd -a nemo systemd-journal
